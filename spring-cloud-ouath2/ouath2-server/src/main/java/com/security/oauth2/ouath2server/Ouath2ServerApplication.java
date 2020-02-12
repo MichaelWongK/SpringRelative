698631dc25767fba6fc92dc7a91837ca16877ec0
MichaelWongK <michael.won007@gmail.com>
2020-02-13 02:32:27 +0800

    feat: add oauth2-server

---
+++ spring-cloud-ouath2/ouath2-server/src/main/java/com/security/oauth2/ouath2server/Ouath2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.security.oauth2.ouath2server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ouath2ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ouath2ServerApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-ouath2/ouath2-server/src/test/java/com/security/oauth2/ouath2server/Ouath2ServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.security.oauth2.ouath2server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ouath2ServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
