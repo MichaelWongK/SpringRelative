9370b16dbcac0a0c1c4012d6b65166723b20cd60
MichaelWongK <michael.won007@gmail.com>
2020-02-14 11:33:49 +0800

    feat: oauth2-client 单点登录及权限校验

---
+++ spring-cloud-ouath2/ouath2-server/oauth2-server/src/main/java/com/security/oauth2/oauth2server/service/UserService.java
@@ -0,0 +1,50 @@
+package com.security.oauth2.oauth2server.service;
+
+import com.security.oauth2.ouath2server.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @Despcription: 用户信息认证
+ * @author micheal.wang <a href="michael.won007@gmail.com"/>
+ * @create 2020-02-12
+ */
+@Service
+public class UserService implements UserDetailsService {
+
+    private List<User> userList;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void initData() {
+        String password = passwordEncoder.encode("123456");
+        userList = new ArrayList<>();
+        userList.add(new User("michael", password, AuthorityUtils.commaSeparatedStringToAuthorityList("admin")));
+        userList.add(new User("andy", password, AuthorityUtils.commaSeparatedStringToAuthorityList("client")));
+        userList.add(new User("mark", password, AuthorityUtils.commaSeparatedStringToAuthorityList("client")));
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<User> findUserList = userList.stream().filter(user -> user.getUsername().equals(username)).collect(Collectors.toList());
+        if (!CollectionUtils.isEmpty(findUserList)) {
+            return findUserList.get(0);
+        } else {
+            throw new UsernameNotFoundException("用户名或密码错误");
+        }
+    }
+}
