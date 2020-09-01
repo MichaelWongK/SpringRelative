44f9d7d12ab05d0fb6a63cabaaa252d9c598e5b6
MichaelWongK <michael.won007@gmail.com>
2020-09-01 22:21:01 +0800

    feat: mysql-master-slave init

---
+++ springboot-mysql/springboot-mysql-master-slave/src/main/java/com/micheal/mysql/springbootmysqlmasterslave/SpringbootMysqlMasterSlaveApplication.java
@@ -0,0 +1,13 @@
+package com.micheal.mysql.springbootmysqlmasterslave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMysqlMasterSlaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMysqlMasterSlaveApplication.class, args);
+    }
+
+}
---
+++ springboot-mysql/springboot-mysql-master-slave/src/test/java/com/micheal/mysql/springbootmysqlmasterslave/SpringbootMysqlMasterSlaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micheal.mysql.springbootmysqlmasterslave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMysqlMasterSlaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
