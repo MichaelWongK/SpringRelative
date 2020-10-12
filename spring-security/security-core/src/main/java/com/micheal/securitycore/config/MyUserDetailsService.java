e69c74fa85fa8cc27a335b00a7f99056bc83c776
MichaelWongK <michael.won007@gmail.com>
2020-10-12 17:39:50 +0800

    feat: delete

---
+++ spring-security/security-core/src/main/java/com/micheal/securitycore/config/MyUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.micheal.securitycore.config;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+
+/**
+ * @author <a href="mailto:wangmk13@163.com">micheal.wang</a>
+ * @date 2020/10/12 17:34
+ * @Description
+ * 自定义MyUserDetailsUserService类，实现 UserDetailsService 接口的 loadUserByUsername()方法，
+ * 这里就简单的返回一个Spring Security 提供的 User 对象
+ */
+public class MyUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        // 不能直接使用 创建 BCryptPasswordEncoder 对象来加密， 这种加密方式 没有 {bcrypt}  前缀，
+        // 会导致在  matches 时导致获取不到加密的算法出现
+        // java.lang.IllegalArgumentException: There is no PasswordEncoder mapped for the id "null"  问题
+        // 问题原因是 Spring Security5 使用 DelegatingPasswordEncoder(委托)  替代 NoOpPasswordEncoder，
+        // 并且 默认使用  BCryptPasswordEncoder 加密（注意 DelegatingPasswordEncoder 委托加密方法BCryptPasswordEncoder  加密前  添加了加密类型的前缀）  https://blog.csdn.net/alinyua/article/details/80219500
+        return new User("user",  PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("123456"), AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+    }
+
+}
