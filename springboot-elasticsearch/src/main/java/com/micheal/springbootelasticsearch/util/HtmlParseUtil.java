6168c204b2cfe970bd3c4a83060c7e8db0712c57
MichaelWongK <michael.won007@gmail.com>
2021-01-26 16:27:17 +0800

    feat: es html update

--- springboot-elasticsearch/src/main/java/com/micheal/springbootelasticsearch/util/HtmlParseUtil.java
+++ springboot-elasticsearch/src/main/java/com/micheal/springbootelasticsearch/util/HtmlParseUtil.java
@@ -20,6 +20,39 @@ public class HtmlParseUtil {
 
     public static void main(String[] args) throws IOException {
 //        new HtmlParseUtil().parseJD("安卓").forEach(System.out::println);
+        parseLOL();
+    }
+
+    public static List<Content> parseLOL() throws IOException {
+        // 获取请求 https://search.jd.com/Search?keyword=java
+        //
+        String url = "https://lol.qq.com/data/info-heros.shtml";
+        // 解析网页 (jsoup返回document对就是浏览器Document对象)
+        Document document = Jsoup.parse(new URL(url), 30000);
+        // 所有js中使用的方法这里都能用
+        Element element = document.getElementById("jSearchHeroDiv");
+        // 获取所有的li元素
+        Elements elements = element.getElementsByTag("li");
+
+        List<Content> goodsList = new ArrayList<>();
+
+        // 获取元素中的内容
+        elements.forEach(el -> {
+            // 图片特别多的网站，所有图片都是延迟加载
+            // data-lazy-img
+            String href = el.getElementsByTag("a").attr("href");
+//            String img = el.getElementsByTag("img").eq(0).attr("data-lazy-img");
+//            String price = el.getElementsByClass("p-price").eq(0).text();
+//            String title = el.getElementsByClass("p-name").eq(0).text();
+//            Content content = new Content();
+//            content.setImg(img);
+//            content.setPrice(price);
+//            content.setTitle(title);
+//            goodsList.add(content);
+            System.out.println(href);
+        });
+
+        return goodsList;
     }
 
     public static List<Content> parseJD(String keyword) throws IOException {
